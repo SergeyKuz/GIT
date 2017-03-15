63a33760de07c62bcdc07597e49a30c577228cf0
sergey2010 <ksd921020@yandex.ru>
2017-03-15 21:58:24 +0500

    Merge branch 'master' of https://github.com/SergeyKuz/GIT
    # Conflicts:
    #	src/main/java/HelloGit.java

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -7,7 +7,7 @@ public class HelloGit {
         System.out.println("Вова программирует");
         System.out.println("Дима программирует");
         ClassDima Dima = new ClassDima(160.0, 74.0);
-ClassVova classVova = new ClassVova(170.0, 84.0);
+        ClassVova classVova = new ClassVova(170.0, 84.0);
         System.out.println(ClassDima.weapon);
     }
 }
